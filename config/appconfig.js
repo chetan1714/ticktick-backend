8d79931017c8df61e576763ba9eed9879c26b9bb
chetan1714 <162134337+chetan1714@users.noreply.github.com>
2025-02-28 01:06:20 +0530

    Update appconfig.js

--- config/appconfig.js
+++ config/appconfig.js
@@ -8,18 +8,18 @@ const appconfig = {
     env: process.env.NODE_ENV || "development",
   },
   db: {
-    MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://chetamn33:Chetan64@cluster0.uglhz.mongodb.net/ticktick?retryWrites=true&w=majority&appName=Cluster0",
+    MONGODB_URI: process.env.MONGODB_URI,
     logging: true,
   },
   winiston: {
     logpath: "/iLrnLogs/logs/",
   },
   auth: {
-    jwt_secret: process.env.JWT_SECRET || "VmVyeVBvd2VyZnVsbFNlY3JldA==",
+    jwt_secret: process.env.JWT_SECRET,
     jwt_expiresin: process.env.JWT_EXPIRES_IN || "7d",
     saltRounds: process.env.SALT_ROUND || 10,
     refresh_token_secret:
-      process.env.REFRESH_TOKEN_SECRET || "VmVyeVBvd2VyZnVsbFNlY3JldA==",
+      process.env.REFRESH_TOKEN_SECRET,
     refresh_token_expiresin: process.env.REFRESH_TOKEN_EXPIRES_IN || "2d", // 2 days
   },
 };
